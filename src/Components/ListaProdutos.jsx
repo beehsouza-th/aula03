a534c92aadbf6701dc5526a28b7dfa7f805db6e5
beehsouza-th <brenda.souza@estudante.ifms.edu.br>
2024-10-28 14:23:50 -0400

    style/module.css

--- src/Components/ListaProdutos.jsx
+++ src/Components/ListaProdutos.jsx
@@ -1,10 +1,12 @@
+import styles from '../styles/listaProdutos.module.css'
 
 export default function ListaProdutos({lista}) {
        return(
-              <main>
-              <h1>Lista de Produtos</h1>
-              <ul>
-              {lista.map(produto => (
+
+            <main>
+                <h1>Lista Produtos</h1>
+               <ul className={styles.blocolista}>
+                 {lista.map(produto => (
                   <li key = {produto.id}>
                       <h2>{produto.title}</h2>
                       <p>{produto.description}</p>
@@ -12,7 +14,7 @@ export default function ListaProdutos({lista}) {
                       <img src={produto.image} alt={produto.title} width={100} />
                       </li>
                   ))}
-                   </ul>
-              </main>
-           )
-       }
+                </ul>
+            </main>
+        );
+}
